7ec18b762fc32395d90cf85b14ae15d7eb533c5c
Nicolas Castejon <nicolas.castejon@protonmail.com>
2024-02-28 20:23:10 +0100

    feature: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
