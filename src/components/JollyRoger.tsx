05fe9d078731fd26bf02b92e467bb4a383dbf7ed
Nicolas Castejon <nicolas.castejon@protonmail.com>
2024-02-28 22:11:23 +0100

    feature: add music and transition

---
+++ src/components/JollyRoger.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import jollyRoger from "../assets/images/jolly-roger-cartoon.png";
+import audioFile from "../assets/audio/drunken-sailor.mp3";
+
+export default function JollyRoger() {
+  const audioRef = React.useRef<HTMLAudioElement>(null);
+  const playSound = () => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.1;
+      audioRef.current.play();
+    }
+  };
+
+  const stopSound = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      // audioRef.current.currentTime = 0; // Rewind to the start
+    }
+  };
+
+  return (
+    <>
+      <a href="/pirates-home" className="peer">
+        <img
+          className="transition ease-in-out duration-500 grayscale hover:grayscale-0 hover:cursor-pointer"
+          src={jollyRoger.src}
+          onMouseEnter={playSound}
+          onMouseLeave={stopSound}
+          alt="Jolly roger figure"
+          width="200"
+          height="200"
+        />
+      </a>
+      <blockquote className="opacity-0 ease-in-out duration-500 peer-hover:opacity-100 text-slate-50 text-lg mt-20">
+        “If there’s a man among ye, ye’ll come up and fight like the man ye are
+        to be!” ― Mary Read
+      </blockquote>
+      <audio ref={audioRef} src={audioFile} />
+    </>
+  );
+}
